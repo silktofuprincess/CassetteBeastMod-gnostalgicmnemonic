259127e9f3f3b8ccbf696b1c9c38f8535f6258c2
silktofuprincess <69822635+silktofuprincess@users.noreply.github.com>
2023-05-24 17:02:50 -0400

    Delete bluh/mods/de_unlimited_romance directory

--- bluh/mods/de_unlimited_romance/uncheck_romanced.gd
+++
@@ -1,9 +0,0 @@
-extends Reference
-
-func start_up():
-	SaveState.connect("flag_changed", self, "check_flag")
-	SaveState.set_flag("player_has_romanced", false)
-
-func check_flag(flag, value):
-	if flag == "player_has_romanced" && value == true:
-		SaveState.set_flag("player_has_romanced", false)
